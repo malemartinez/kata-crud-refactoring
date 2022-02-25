ccaeca8fb20087faa38d30e943b6f96a2e011006
Aleja Martinez <martinezmayra605@gmail.com>
2022-02-25 09:44:51 -0500

    Adición de model tanto del toDo como de List como forma para hacer DTO

---
+++ Backend-ToDos/src/main/java/com/Alejandra/ToDos/Model/ListModel.java
@@ -0,0 +1,14 @@
+package com.Alejandra.ToDos.Model;
+
+import com.Alejandra.ToDos.Entity.ToDoEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListModel {
+
+    private Long id;
+    private String name;
+    private List<ToDoEntity> Todos = new ArrayList<>();
+
+}
---
+++ Backend-ToDos/src/main/java/com/Alejandra/ToDos/Model/ToDoModel.java
@@ -0,0 +1,10 @@
+package com.Alejandra.ToDos.Model;
+
+public class ToDoModel {
+
+    private long id;
+    private String name;
+    private boolean Completed;
+    private Long listID;
+
+}
