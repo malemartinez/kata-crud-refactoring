cb4735f81bf86aab44c9eb9aba4fffa0c7b44d0a
Aleja Martinez <martinezmayra605@gmail.com>
2022-02-25 21:29:03 -0500

    Adicion de servicios a la Api. Adicion de la estuctura del componente

--- src/componentes/CategoryList.jsx
+++ src/componentes/CategoryList.jsx
@@ -1,7 +1,9 @@
 import { useContext ,useEffect } from 'react';
 import { ListStore } from './ListStore';
-import { HOST_API } from '../servicios/Url';
 import DataService from "../servicios/servicios-HTTP"
+import { StoreProvider } from './Store';
+import List from './List';
+import {Form} from "./ToDoCreator"
 
 const CategoryList = () => {
   const { dispatch, state: { listCategory } } = useContext(ListStore);
@@ -10,32 +12,39 @@ const CategoryList = () => {
   useEffect(() => {
     //peticion al servidor para que traiga todas las listas creadas
     DataService.getAll()
-      .then((list) => {
-        dispatch({ type: "update-list",list: list.data }) //agrega los items ed la peticion a list
-        console.log(list.data)
+      .then((response) => {
+        dispatch({ type: "update-list",list: response.data }) //agrega los items ed la peticion a list
+        console.log(response.data)
       })
   }, [dispatch]); //El dispatch me indica que solo se ejecute de nuevo el useEfecto cuando el dispatch cambie
 
 
-  // const onEdit = (todo) => {
-  //   dispatch({ type: "edit-item", item: todo })
-  // };
-  // DataService.updateCompleted()
-  //     .then(response => response.json())
-  //     .then((todo) => {
-  //       dispatch({ type: "update-item", item: todo });
-    
-  //     })
+  //Eliminar la lista completa
+  const onDelete = (id)=>{
+    DataService.delete(id).then((list) => {
+      dispatch({ type: "delete-item", id })
+    })
+  }
 
   return (
     <div>
       {
           currentList.map(itemCategory =>{
-            <div>
-              <h5> {itemCategory.name} </h5>
-              <button>Eliminar</button>
-              <button>Editar</button>
-            </div>
+            return(
+              <div>
+                <div>
+                  <h3 key={itemCategory.id}> {itemCategory.name} </h3>
+                  <button onClick={()=> onDelete(itemCategory.id)} >Eliminar</button>
+                </div>
+                <StoreProvider>
+                  <Form Category = {itemCategory.id} />
+                  <List  Category = {itemCategory.id}/>
+                </StoreProvider>
+                <hr></hr>
+
+              </div>
+            )
+            
           })
       }
       
