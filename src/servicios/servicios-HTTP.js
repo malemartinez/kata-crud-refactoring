fc9686980ded911be2b618e7763c25f4a8b6a113
Aleja Martinez <martinezmayra605@gmail.com>
2022-02-25 21:24:21 -0500

    Adicion de acciones CRUD para la conexion a la Api

--- src/servicios/servicios-HTTP.js
+++ src/servicios/servicios-HTTP.js
@@ -2,20 +2,46 @@
 import http from "./axios";
 
 class DataService {
+
+  //Traer todas las listas creadas
   getAll() {
     return http.get("/list");
   }
 
+  //traer todos los ToDos de una lista especifica
   get(id) {
-    return http.get(`/${id}`);
+    return http.get(`list/todos/${id}`);
   }
 
+  //Crear una lista
   create(data) {
     return http.post("/list", data);
   }
 
+  //crear ToDo con idList
+  createToDo(id , data){
+    return http.post(`/todo/${id}`, data)
+  }
+
+  //Eliminar una lista en especifico
   delete(id) {
-    return http.delete(`/${id}`);
+    return http.delete(`/list/${id}`);
+  }
+
+  //Eliminar un ToDo especifico
+  deleteToDo(id){
+    return http.delete(`todo/${id}`)
+  }
+
+
+  //Actualizar ToDo
+  updateToDo(id, data){
+    return http.put(`/todo/${id}`, data)
+  }
+
+  //Actualizar nombre de la lista
+  updateList(id, data){
+    return http.put(`/list/${id}`,data)
   }
 
   deleteAll() {
